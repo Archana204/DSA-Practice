da38955d6a58a953ebc3362f12031626e5accc6e
Archana204 <165003542+Archana204@users.noreply.github.com>
2026-03-04 19:55:58 +0530

    Implement max element finder in an array

--- Arrays/max_element.java
+++ Arrays/max_element.java
@@ -1,1 +1,16 @@
-
+//Problem: Max Element in an Array
+public class MaxElement {
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 45, 2, 99, 23};
+        System.out.println("Maximum Element: " + findMax(arr));
+    }
+}
