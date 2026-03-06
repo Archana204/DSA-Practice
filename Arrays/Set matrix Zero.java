f8ea8b25721280327d5a0a275e458a79eef93b5c
Archana Bojjapally <165003542+Archana204@users.noreply.github.com>
2026-03-06 17:00:30 +0530

    Add functionality to zero out rows and columns in matrix
    Implement logic to set entire row and column to zero if a zero is found in the matrix.

--- Arrays/Set matrix Zero.java
+++ Arrays/Set matrix Zero.java
@@ -1,1 +1,39 @@
-
+//Problem statement : When ever their is Zero in a row/column then the complete row/column and corresponding row/column should be zero.
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int mat[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mat[i][j]=sc.nextInt();
+            }
+        }
+        int row[]=new int[n];
+        int col[]=new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==1|| col[j]==1){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
