332fa040577f20df07cdc969e7ca7c8152d9b708
Archana Bojjapally <165003542+Archana204@users.noreply.github.com>
2026-03-07 18:12:12 +0530

    Add method to remove duplicates from linked list
    Implemented a method to remove duplicate elements from a linked list.

---
+++ Linked List/Remove duplicate elements.java
@@ -0,0 +1,15 @@
+//Problem Statement: To Remove Duplicate Elements from the List.
+class Solution {
+    public ListNode removedup(ListNode head) {
+        ListNode temp=head;
+        while(temp!= null && temp.next != null){
+            if(temp.val==temp.next.val){
+                temp.next=temp.next.next;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
